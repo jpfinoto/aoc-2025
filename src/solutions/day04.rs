ac6b34572d5203926802ad782311c8235a0390ac
jp <jp@finoto.dev>
2025-12-04 09:03:55 +0100

    refactor(day04): rename variables and reorganise code

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -3,13 +3,13 @@ use crate::utils::grid::{DenseGrid, XY};
 use derive_solution::{parser, solution};
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum CellState {
+pub enum Cell {
     PaperRoll,
     Empty,
 }
 
 #[parser]
-fn parse_input(input: &PuzzleInput) -> DenseGrid<CellState> {
+fn parse_input(input: &PuzzleInput) -> DenseGrid<Cell> {
     DenseGrid::from_rows(
         input
             .get_lines()
@@ -17,8 +17,8 @@ fn parse_input(input: &PuzzleInput) -> DenseGrid<CellState> {
             .map(|line| {
                 line.chars()
                     .map(|c| match c {
-                        '.' => CellState::Empty,
-                        '@' => CellState::PaperRoll,
+                        '.' => Cell::Empty,
+                        '@' => Cell::PaperRoll,
                         _ => unreachable!(),
                     })
                     .collect()
@@ -28,15 +28,15 @@ fn parse_input(input: &PuzzleInput) -> DenseGrid<CellState> {
 }
 
 #[solution(day = 4, part = 1)]
-fn solve_part_1(input: DenseGrid<CellState>) -> usize {
-    get_removable_paper_rolls(&input).count()
+fn solve_part_1(grid: DenseGrid<Cell>) -> usize {
+    get_removable_paper_rolls(&grid).count()
 }
 
 #[solution(day = 4, part = 2)]
-fn solve_part_2(mut input: DenseGrid<CellState>) -> usize {
+fn solve_part_2(mut grid: DenseGrid<Cell>) -> usize {
     let mut total_removed = 0;
     loop {
-        let removable_rolls: Vec<_> = get_removable_paper_rolls(&input).collect();
+        let removable_rolls: Vec<_> = get_removable_paper_rolls(&grid).collect();
 
         if removable_rolls.is_empty() {
             break;
@@ -44,28 +44,25 @@ fn solve_part_2(mut input: DenseGrid<CellState>) -> usize {
 
         total_removed += removable_rolls.len();
         removable_rolls.into_iter().for_each(|pos| {
-            input.set_at(pos, CellState::Empty);
+            grid.set_at(pos, Cell::Empty);
         });
     }
 
     total_removed
 }
 
-fn get_removable_paper_rolls(grid: &DenseGrid<CellState>) -> impl Iterator<Item = XY> + '_ {
+fn get_removable_paper_rolls(grid: &DenseGrid<Cell>) -> impl Iterator<Item = XY> + '_ {
     grid.items()
         .filter(|&(pos, state)| {
-            if matches!(state, CellState::PaperRoll) {
-                let paper_roll_neighbours = pos
-                    .all_neighbours()
-                    .filter(|pos2| matches!(grid.at(*pos2), Some(CellState::PaperRoll)))
-                    .count();
-
-                paper_roll_neighbours < 4
-            } else {
-                false
-            }
+            matches!(state, Cell::PaperRoll) && count_neighbour_paper_rolls(grid, pos) < 4
         })
         .map(|(pos, _)| pos)
+}
+
+fn count_neighbour_paper_rolls(grid: &DenseGrid<Cell>, pos: XY) -> usize {
+    pos.all_neighbours()
+        .filter(|pos2| matches!(grid.at(*pos2), Some(Cell::PaperRoll)))
+        .count()
 }
 
 #[cfg(test)]
